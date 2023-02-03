d8750439fca34a8f336cbc63e6dbf68cffd3abfb
Richard Stotz <richardstotz@google.com>
2023-02-03 08:34:06 -0800

    Add Windows add C++20 build rules
    PiperOrigin-RevId: 506910836

--- tools/test_windows.sh
+++ tools/test_windows.sh
@@ -14,40 +14,37 @@
 # limitations under the License.
 
 
-# Compile and runs the unit tests on Windows
+# Compile and runs the unit tests on Windows and C++20.
 #
 # Build and run the compatible test of YDF without tensorflow support. 
 #
+# This script is tested with MSVC 2017 and C++20. Other compilers and C++
+# versions are not supported as of yet, but patches are welcome. Notably,
+# earlier C++ versions are not compatible with MSVC and the current abseil due
+# to its use of designated initializers.
+#
 # Options:
-#  CPP_VERSIONS: C++ Versions to build, separated by semicolon. Can be 14 or 17.
-#                Defaults to 17
 #  RUN_TESTS: Run the unit tests, 0 or 1 (default).
 #
 # Usage example:
 #
-#   # Compilation with C++17. running tests.
-#   ./tools/test_bazel.sh
-#    
-#   # Compilation with C++14 and C++17, no tests run.
-#   CPP_VERSIONS="14;17" RUN_TESTS=0 ./tools/test_bazel.sh
+#   # Compilation with C++20. running tests.
+#   ./tools/test_windows.sh
 #
 set -xev
 
 build_and_maybe_test () {
-   echo "Building YDF the following settings:"
-   echo "   C++ Version: $1"
+   echo "Start building YDF"
 
     BAZEL=bazel
     ${BAZEL} version
 
-    local flags="--config=windows_cpp${1} --config=windows_avx2 --features=-fully_static_link --repo_env=CC=${2}"
+    local flags="--config=windows_cpp20 --config=windows_avx2 --features=-fully_static_link"
     # Not all tests can be run without TF support
-    local testable_components=""
-    local buildable_cli_components=""
+    local testable_components=":all"
+    local buildable_cli_components="metric/...:all"
     # No tensorflow support
     cp -f WORKSPACE_NO_TF WORKSPACE
-    buildable_cli_components=":all"
-    testable_components="metric/...:all"
 
     time ${BAZEL} build //yggdrasil_decision_forests/cli${buildable_cli_components}  //examples:beginner_cc ${flags}
     if [ "$RUN_TESTS" = 1 ]; then
@@ -58,17 +55,12 @@ build_and_maybe_test () {
 
 main () {
   # Set default values
-  : "${CPP_VERSIONS:=17}"
   : "${RUN_TESTS:=1}"
 
   # Adjust bazelrc to prevent long paths issue
   echo "startup --output_user_root=${TMPDIR}" >> ".bazelrc"
 
-  local cpp_version_array=(${CPP_VERSIONS//;/ })
-
-  for cpp_version in ${cpp_version_array[@]}; do
-    build_and_maybe_test $cpp_version
-  done
+  build_and_maybe_test $cpp_version
 }
 
 main
