4a8d03363319d7f51c0188e8cf0c0068c9c02a20
Richard Stotz <richardstotz@google.com>
2023-11-20 03:34:53 -0800

    Fix a misplaced comment
    PiperOrigin-RevId: 583974850

--- yggdrasil_decision_forests/dataset/vertical_dataset_io.h
+++ yggdrasil_decision_forests/dataset/vertical_dataset_io.h
@@ -22,15 +22,29 @@
 #ifndef YGGDRASIL_DECISION_FORESTS_DATASET_VERTICAL_DATASET_IO_H_
 #define YGGDRASIL_DECISION_FORESTS_DATASET_VERTICAL_DATASET_IO_H_
 
+#include <cstdint>
+#include <functional>
 #include <vector>
 
 #include "absl/status/status.h"
 #include "absl/strings/string_view.h"
+#include "absl/types/optional.h"
 #include "yggdrasil_decision_forests/dataset/data_spec.pb.h"
 #include "yggdrasil_decision_forests/dataset/vertical_dataset.h"
 
 namespace yggdrasil_decision_forests {
 namespace dataset {
+
+// Advanced options for dataset loading.
+struct LoadConfig {
+  // Number of reading threads. Only used for multi-sharded datasets.
+  // num_threads=1 is more memory efficient than num_threads>1.
+  int num_threads = 10;
+  // If specified, only load this subset of columns.
+  absl::optional<std::vector<int>> load_columns;
+  // If specified, only load the examples that evaluate to true.
+  absl::optional<std::function<bool(const proto::Example&)>> load_example;
+};
 
 // Load the dataset content from a file (or a set of files).
 //
@@ -49,21 +63,8 @@ namespace dataset {
 //     dataset. required_columns: Control if non existing features are
 //     allowed. See comment above for the specific semantic of
 //     "required_columns".
-//   num_threads: Number of reading threads. Only used for multi-sharded
-//     datasets. num_threads=1 is more memory efficient than num_threads>1.
-//   load_columns: If specified, only load this subset of columns.
-//   load_example: If specified, only load the examples that evaluate to true.
+//   config: Optional additional configuration options.
 //
-struct LoadConfig {
-  // Number of reading threads. Only used for multi-sharded datasets.
-  // num_threads=1 is more memory efficient than num_threads>1.
-  int num_threads = 10;
-  // If specified, only load this subset of columns.
-  absl::optional<std::vector<int>> load_columns;
-  // If specified, only load the examples that evaluate to true.
-  absl::optional<std::function<bool(const proto::Example&)>> load_example;
-};
-
 absl::Status LoadVerticalDataset(
     absl::string_view typed_path, const proto::DataSpecification& data_spec,
     VerticalDataset* dataset,
