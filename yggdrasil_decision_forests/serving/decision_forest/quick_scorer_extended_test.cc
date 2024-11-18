53395b2126d7c0ef7c81e46c023d505ce8080ed0
Richard Stotz <richardstotz@google.com>
2024-11-18 05:50:32 -0800

    [YDF] Additional tests for QuickScorer engine
    PiperOrigin-RevId: 697596253

--- yggdrasil_decision_forests/serving/decision_forest/quick_scorer_extended_test.cc
+++ yggdrasil_decision_forests/serving/decision_forest/quick_scorer_extended_test.cc
@@ -13,14 +13,21 @@
  * limitations under the License.
  */
 
+#include <limits>
 #include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "absl/log/check.h"
 #include "absl/log/log.h"
+#include "yggdrasil_decision_forests/dataset/data_spec.pb.h"
+#include "yggdrasil_decision_forests/dataset/vertical_dataset.h"
 #include "yggdrasil_decision_forests/model/decision_tree/decision_tree.h"
 #include "yggdrasil_decision_forests/model/gradient_boosted_trees/gradient_boosted_trees.h"
+#include "yggdrasil_decision_forests/serving/example_set.h"
 #include "yggdrasil_decision_forests/utils/test.h"
 
 #include "yggdrasil_decision_forests/serving/decision_forest/quick_scorer_extended.h"
@@ -37,35 +44,44 @@ using model::gradient_boosted_trees::GradientBoostedTreesModel;
 using model::gradient_boosted_trees::proto::Loss;
 using testing::ElementsAre;
 
-void BuildToyModelAndToyDataset(const model::proto::Task task,
-                                const bool use_cateset_feature,
-                                GradientBoostedTreesModel* model,
-                                dataset::VerticalDataset* dataset,
-                                const int duplicate_factor = 1) {
-  dataset::proto::DataSpecification dataspec = PARSE_TEST_PROTO(R"pb(
-    columns { type: NUMERICAL name: "a" }
-    columns { type: NUMERICAL name: "b" }
-    columns {
-      type: CATEGORICAL
-      name: "c"
-      categorical { is_already_integerized: true number_of_unique_values: 4 }
+void BuildToyModelAndToyDataset(
+    const model::proto::Task task, const bool use_num_feature,
+    const bool use_cat_feature, const bool use_discnum_feature,
+    const bool use_catset_feature, GradientBoostedTreesModel* model,
+    dataset::VerticalDataset* dataset, const int duplicate_factor = 1) {
+  dataset::proto::DataSpecification dataspec;
+  if (use_num_feature) {
+    {
+      auto* d = dataspec.add_columns();
+      d->set_name("num_1");
+      d->set_type(dataset::proto::ColumnType::NUMERICAL);
     }
-    columns {
-      type: DISCRETIZED_NUMERICAL
-      name: "e"
-      numerical { mean: -1 }
-      discretized_numerical {
-        boundaries: 0.0
-        boundaries: 0.1
-        boundaries: 0.2
-        boundaries: 0.3
-      }
+    {
+      auto* d = dataspec.add_columns();
+      d->set_name("num_2");
+      d->set_type(dataset::proto::ColumnType::NUMERICAL);
     }
-  )pb");
-
-  if (use_cateset_feature) {
+  }
+  if (use_cat_feature) {
     auto* d = dataspec.add_columns();
-    d->set_name("d");
+    d->set_name("cat");
+    d->set_type(dataset::proto::ColumnType::CATEGORICAL);
+    d->mutable_categorical()->set_is_already_integerized(true);
+    d->mutable_categorical()->set_number_of_unique_values(4);
+  }
+  if (use_discnum_feature) {
+    auto* d = dataspec.add_columns();
+    d->set_name("disc_num");
+    d->set_type(dataset::proto::ColumnType::DISCRETIZED_NUMERICAL);
+    d->mutable_numerical()->set_mean(-1);
+    d->mutable_discretized_numerical()->add_boundaries(0.0);
+    d->mutable_discretized_numerical()->add_boundaries(0.1);
+    d->mutable_discretized_numerical()->add_boundaries(0.2);
+    d->mutable_discretized_numerical()->add_boundaries(0.3);
+  }
+  if (use_catset_feature) {
+    auto* d = dataspec.add_columns();
+    d->set_name("cat_set");
     d->set_type(dataset::proto::ColumnType::CATEGORICAL_SET);
     d->mutable_categorical()->set_is_already_integerized(false);
     d->mutable_categorical()->set_number_of_unique_values(4);
@@ -77,15 +93,23 @@ void BuildToyModelAndToyDataset(const model::proto::Task task,
   }
 
   dataset->set_data_spec(dataspec);
-  CHECK_OK(dataset->CreateColumnsFromDataspec());
+  ASSERT_OK(dataset->CreateColumnsFromDataspec());
 
-  if (use_cateset_feature) {
-    CHECK_OK(dataset->AppendExampleWithStatus(
-        {{"a", "0.5"}, {"b", "0.5"}, {"c", "1"}, {"e", "1.5"}, {"d", "2 3"}}));
-  } else {
-    CHECK_OK(dataset->AppendExampleWithStatus(
-        {{"a", "0.5"}, {"b", "0.5"}, {"c", "1"}, {"e", "1.5"}}));
+  std::unordered_map<std::string, std::string> example;
+  if (use_num_feature) {
+    example["num_1"] = "0.5";
+    example["num_2"] = "0.5";
   }
+  if (use_cat_feature) {
+    example["cat"] = "1";
+  }
+  if (use_discnum_feature) {
+    example["disc_num"] = "1.5";
+  }
+  if (use_catset_feature) {
+    example["cat_set"] = "2 3";
+  }
+  ASSERT_OK(dataset->AppendExampleWithStatus(example));
 
   struct NodeHelper {
     Condition* condition;
@@ -114,7 +138,7 @@ void BuildToyModelAndToyDataset(const model::proto::Task task,
 
   for (int duplication_idx = 0; duplication_idx < duplicate_factor;
        duplication_idx++) {
-    {
+    if (use_num_feature) {
       auto tree = std::make_unique<DecisionTree>();
       tree->CreateRoot();
       auto n1 = split_node(tree->mutable_root(), 1);
@@ -132,8 +156,7 @@ void BuildToyModelAndToyDataset(const model::proto::Task task,
 
       model->mutable_decision_trees()->push_back(std::move(tree));
     }
-
-    {
+    if (use_cat_feature && use_num_feature) {
       auto tree = std::make_unique<DecisionTree>();
       tree->CreateRoot();
       auto n1 = split_node(tree->mutable_root(), 2);
@@ -153,7 +176,7 @@ void BuildToyModelAndToyDataset(const model::proto::Task task,
       model->mutable_decision_trees()->push_back(std::move(tree));
     }
 
-    {
+    if (use_cat_feature && use_num_feature) {
       auto tree = std::make_unique<DecisionTree>();
       tree->CreateRoot();
       auto n1 = split_node(tree->mutable_root(), 1);
@@ -174,7 +197,7 @@ void BuildToyModelAndToyDataset(const model::proto::Task task,
       model->mutable_decision_trees()->push_back(std::move(tree));
     }
 
-    {
+    if (use_discnum_feature) {
       auto tree = std::make_unique<DecisionTree>();
       tree->CreateRoot();
       auto n1 = split_node(tree->mutable_root(), 3);
@@ -185,7 +208,7 @@ void BuildToyModelAndToyDataset(const model::proto::Task task,
       model->mutable_decision_trees()->push_back(std::move(tree));
     }
 
-    if (use_cateset_feature) {
+    if (use_catset_feature) {
       auto tree = std::make_unique<DecisionTree>();
       tree->CreateRoot();
       auto n1 = split_node(tree->mutable_root(), 4);
@@ -206,13 +229,13 @@ void BuildToyModelAndToyDataset(const model::proto::Task task,
   }
 }
 
-TEST(QuickScorer, Compilation) {
+TEST(QuickScorer, Compilation_Num_Cat_DiscNum) {
   GradientBoostedTreesModel model;
   dataset::VerticalDataset dataset;
-  BuildToyModelAndToyDataset(model::proto::Task::REGRESSION,
-                             /*use_cateset_feature=*/false, &model, &dataset);
+  BuildToyModelAndToyDataset(model::proto::Task::REGRESSION, true, true, true,
+                             /*use_catset_feature=*/false, &model, &dataset);
   GradientBoostedTreesRegressionQuickScorerExtended quick_scorer_model;
-  CHECK_OK(GenericToSpecializedModel(model, &quick_scorer_model));
+  ASSERT_OK(GenericToSpecializedModel(model, &quick_scorer_model));
 
   const auto model_description = DescribeQuickScorer(quick_scorer_model);
   LOG(INFO) << "Model:\n" << model_description;
@@ -250,13 +273,155 @@ TEST(QuickScorer, Compilation) {
                           1 + 1 + 10 + 300 + 20000));
 }
 
+TEST(QuickScorer, Compilation_Num) {
+  GradientBoostedTreesModel model;
+  dataset::VerticalDataset dataset;
+  BuildToyModelAndToyDataset(
+      model::proto::Task::REGRESSION, /*use_num_feature=*/true,
+      /*use_cat_feature=*/false, /*use_discnum_feature=*/false,
+      /*use_catset_feature=*/false, &model, &dataset);
+  GradientBoostedTreesRegressionQuickScorerExtended quick_scorer_model;
+  ASSERT_OK(GenericToSpecializedModel(model, &quick_scorer_model));
+
+  const auto model_description = DescribeQuickScorer(quick_scorer_model);
+  LOG(INFO) << "Model:\n" << model_description;
+
+  EXPECT_EQ(quick_scorer_model.features().input_features().size(), 1);
+
+  // Examples in FORMAT_FEATURE_MAJOR, see decision_forest.h.
+  using V = NumericalOrCategoricalValue;
+  std::vector<V> examples = {
+      // Feature 1
+      V::Numerical(0.5f), V::Numerical(1.0f), V::Numerical(1.5f),
+      V::Numerical(2.5f), V::Numerical(3.5f),
+  };
+  std::vector<float> predictions;
+  PredictQuickScorer(quick_scorer_model, examples, 5, &predictions);
+
+  EXPECT_THAT(predictions, ElementsAre(1 + 4, 1 + 3, 1 + 3, 1 + 2, 1 + 1));
+}
+
+TEST(QuickScorer, Compilation_Num_Nan) {
+  struct NodeHelper {
+    Condition* condition;
+    NodeWithChildren* pos;
+    NodeWithChildren* neg;
+    NodeWithChildren* node;
+  };
+
+  const auto split_node = [](NodeWithChildren* node,
+                             const int attribute) -> NodeHelper {
+    node->CreateChildren();
+    node->mutable_node()->mutable_condition()->set_attribute(attribute);
+    return {/*.condition =*/
+            node->mutable_node()->mutable_condition()->mutable_condition(),
+            /*.pos =*/node->mutable_pos_child(),
+            /*.neg =*/node->mutable_neg_child(),
+            /*.node =*/node};
+  };
+
+  GradientBoostedTreesModel model;
+  dataset::VerticalDataset dataset;
+  dataset::proto::DataSpecification dataspec;
+  auto* d_label = dataspec.add_columns();
+  d_label->set_name("label");
+  d_label->set_type(dataset::proto::ColumnType::NUMERICAL);
+  auto* d_num = dataspec.add_columns();
+  d_num->set_name("num");
+  d_num->set_type(dataset::proto::ColumnType::NUMERICAL);
+  dataset.set_data_spec(dataspec);
+  ASSERT_OK(dataset.CreateColumnsFromDataspec());
+
+  model.set_task(model::proto::Task::REGRESSION);
+  model.set_label_col_idx(0);
+  model.set_data_spec(dataspec);
+  model::gradient_boosted_trees::proto::LossConfiguration loss_config;
+  model.set_loss(Loss::SQUARED_ERROR, loss_config);
+  model.mutable_initial_predictions()->push_back(1.f);
+
+  auto tree = std::make_unique<DecisionTree>();
+  tree->CreateRoot();
+  auto n1 = split_node(tree->mutable_root(), 1);
+  n1.condition->mutable_higher_condition()->set_threshold(2.0f);
+  n1.node->mutable_node()->mutable_condition()->set_na_value(true);
+  n1.neg->mutable_node()->mutable_regressor()->set_top_value(1.f);
+  n1.pos->mutable_node()->mutable_regressor()->set_top_value(2.f);
+  model.mutable_decision_trees()->push_back(std::move(tree));
+
+  GradientBoostedTreesRegressionQuickScorerExtended quick_scorer_model;
+  ASSERT_OK(GenericToSpecializedModel(model, &quick_scorer_model));
+
+  EXPECT_FALSE(quick_scorer_model.global_imputation_optimization);
+
+  const auto model_description = DescribeQuickScorer(quick_scorer_model);
+  LOG(INFO) << "Model:\n" << model_description;
+
+  EXPECT_EQ(quick_scorer_model.features().input_features().size(), 1);
+
+  // Examples in FORMAT_FEATURE_MAJOR, see decision_forest.h.
+  using V = NumericalOrCategoricalValue;
+  std::vector<V> examples = {
+      // Feature 1
+      V::Numerical(0.5f),
+      V::Numerical(std::numeric_limits<float>::quiet_NaN()),
+      V::Numerical(3.5f),
+      V::Numerical(std::numeric_limits<float>::quiet_NaN()),
+      V::Numerical(std::numeric_limits<float>::quiet_NaN()),
+  };
+  std::vector<float> predictions;
+  PredictQuickScorer(quick_scorer_model, examples, 5, &predictions);
+
+  EXPECT_THAT(predictions, ElementsAre(1 + 1, 1 + 2, 1 + 2, 1 + 2, 1 + 2));
+}
+
+TEST(QuickScorer, Compilation_Num_Cat) {
+  GradientBoostedTreesModel model;
+  dataset::VerticalDataset dataset;
+  BuildToyModelAndToyDataset(model::proto::Task::REGRESSION,
+                             /*use_num_feature=*/true, /*use_cat_feature=*/true,
+                             /*use_discnum_feature=*/false,
+                             /*use_catset_feature=*/false, &model, &dataset);
+  GradientBoostedTreesRegressionQuickScorerExtended quick_scorer_model;
+  ASSERT_OK(GenericToSpecializedModel(model, &quick_scorer_model));
+
+  const auto model_description = DescribeQuickScorer(quick_scorer_model);
+  LOG(INFO) << "Model:\n" << model_description;
+
+  ASSERT_EQ(quick_scorer_model.features().input_features().size(), 2);
+
+  // Examples in FORMAT_FEATURE_MAJOR, see decision_forest.h.
+  using V = NumericalOrCategoricalValue;
+  std::vector<V> examples = {
+      // Feature 1
+      V::Numerical(0.5f),
+      V::Numerical(1.0f),
+      V::Numerical(1.5f),
+      V::Numerical(2.5f),
+      V::Numerical(3.5f),
+      // Feature 2
+      V::Categorical(0),
+      V::Categorical(1),
+      V::Categorical(2),
+      V::Categorical(0),
+      V::Categorical(1),
+  };
+  std::vector<float> predictions;
+  PredictQuickScorer(quick_scorer_model, examples, 5, &predictions);
+
+  EXPECT_THAT(predictions,
+              ElementsAre(1 + 4 + 40 + 400, 1 + 3 + 20 + 300, 1 + 3 + 20 + 400,
+                          1 + 2 + 30 + 400, 1 + 1 + 10 + 300));
+}
+
 TEST(QuickScorer, ExampleSet) {
   GradientBoostedTreesModel model;
   dataset::VerticalDataset dataset;
   BuildToyModelAndToyDataset(model::proto::Task::REGRESSION,
-                             /*use_cateset_feature=*/true, &model, &dataset);
+                             /*use_num_feature=*/true, /*use_cat_feature=*/true,
+                             /*use_discnum_feature=*/true,
+                             /*use_catset_feature=*/true, &model, &dataset);
   GradientBoostedTreesRegressionQuickScorerExtended quick_scorer_model;
-  CHECK_OK(GenericToSpecializedModel(model, &quick_scorer_model));
+  ASSERT_OK(GenericToSpecializedModel(model, &quick_scorer_model));
 
   const auto model_description = DescribeQuickScorer(quick_scorer_model);
   LOG(INFO) << "Model:\n" << model_description;
@@ -269,19 +434,19 @@ TEST(QuickScorer, ExampleSet) {
 
   const auto feature_1 =
       GradientBoostedTreesRegressionQuickScorerExtended::ExampleSet::
-          GetNumericalFeatureId("b", quick_scorer_model)
+          GetNumericalFeatureId("num_2", quick_scorer_model)
               .value();
   const auto feature_2 =
       GradientBoostedTreesRegressionQuickScorerExtended::ExampleSet::
-          GetCategoricalFeatureId("c", quick_scorer_model)
+          GetCategoricalFeatureId("cat", quick_scorer_model)
               .value();
   const auto feature_3 =
       GradientBoostedTreesRegressionQuickScorerExtended::ExampleSet::
-          GetCategoricalSetFeatureId("d", quick_scorer_model)
+          GetCategoricalSetFeatureId("cat_set", quick_scorer_model)
               .value();
   const auto feature_4 =
       GradientBoostedTreesRegressionQuickScorerExtended::ExampleSet::
-          GetNumericalFeatureId("e", quick_scorer_model)
+          GetNumericalFeatureId("disc_num", quick_scorer_model)
               .value();
 
   examples.SetNumerical(0, feature_1, 0.5f, quick_scorer_model);
@@ -325,11 +490,12 @@ TEST(QuickScorer, ExceedStackBuffer) {
   GradientBoostedTreesModel model;
 
   dataset::VerticalDataset dataset;
-  BuildToyModelAndToyDataset(model::proto::Task::REGRESSION,
-                             /*use_cateset_feature=*/true, &model, &dataset,
-                             duplicate_factor);
+  BuildToyModelAndToyDataset(
+      model::proto::Task::REGRESSION, /*use_num_feature=*/true,
+      /*use_cat_feature=*/true, /*use_discnum_feature=*/true,
+      /*use_catset_feature=*/true, &model, &dataset, duplicate_factor);
   GradientBoostedTreesRegressionQuickScorerExtended quick_scorer_model;
-  CHECK_OK(GenericToSpecializedModel(model, &quick_scorer_model));
+  ASSERT_OK(GenericToSpecializedModel(model, &quick_scorer_model));
 
   const auto model_description = DescribeQuickScorer(quick_scorer_model);
   LOG(INFO) << "Model:\n" << model_description;
@@ -342,20 +508,20 @@ TEST(QuickScorer, ExceedStackBuffer) {
 
   const auto feature_1 =
       GradientBoostedTreesRegressionQuickScorerExtended::ExampleSet::
-          GetNumericalFeatureId("b", quick_scorer_model)
+          GetNumericalFeatureId("num_2", quick_scorer_model)
               .value();
   const auto feature_2 =
       GradientBoostedTreesRegressionQuickScorerExtended::ExampleSet::
-          GetCategoricalFeatureId("c", quick_scorer_model)
+          GetCategoricalFeatureId("cat", quick_scorer_model)
               .value();
   const auto feature_3 =
       GradientBoostedTreesRegressionQuickScorerExtended::ExampleSet::
-          GetCategoricalSetFeatureId("d", quick_scorer_model)
+          GetCategoricalSetFeatureId("cat_set", quick_scorer_model)
               .value();
 
   const auto feature_4 =
       GradientBoostedTreesRegressionQuickScorerExtended::ExampleSet::
-          GetNumericalFeatureId("e", quick_scorer_model)
+          GetNumericalFeatureId("disc_num", quick_scorer_model)
               .value();
 
   examples.SetNumerical(0, feature_1, 0.5f, quick_scorer_model);
