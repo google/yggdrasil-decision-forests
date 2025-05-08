3e5b8eb9c86ddfead142dfd1e24d476ed9b1cb59
Mathieu Guillame-Bert <gbm@google.com>
2025-05-08 01:29:16 -0700

    SHAP values; Part 2
    PiperOrigin-RevId: 756202897

--- yggdrasil_decision_forests/utils/shap.cc
+++ yggdrasil_decision_forests/utils/shap.cc
@@ -16,11 +16,74 @@
 #include "yggdrasil_decision_forests/utils/shap.h"
 
 #include <cmath>
+#include <optional>
+#include <string>
+#include <utility>
 #include <vector>
 
 #include "absl/log/check.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_format.h"
+#include "absl/strings/str_join.h"
+#include "yggdrasil_decision_forests/dataset/example.pb.h"
+#include "yggdrasil_decision_forests/model/abstract_model.h"
+#include "yggdrasil_decision_forests/model/decision_tree/decision_forest_interface.h"
+#include "yggdrasil_decision_forests/model/decision_tree/decision_tree.h"
+#include "yggdrasil_decision_forests/model/decision_tree/decision_tree.pb.h"
+#include "yggdrasil_decision_forests/model/gradient_boosted_trees/gradient_boosted_trees.h"
+#include "yggdrasil_decision_forests/model/random_forest/random_forest.h"
+#include "yggdrasil_decision_forests/utils/status_macros.h"
 
 namespace yggdrasil_decision_forests::utils::shap {
+
+namespace {
+
+// Get the number of training examples in a node.
+double GetNumNodeExamples(const model::decision_tree::NodeWithChildren& node) {
+  // TODO: Add support for weighted trainings. Need to work for non-leaf
+  // nodes.
+  DCHECK(node.node().has_num_pos_training_examples_without_weight());
+  return node.node().num_pos_training_examples_without_weight();
+}
+
+double GetRegressionNodeValue(
+    const model::decision_tree::NodeWithChildren& node, const int output_idx) {
+  DCHECK_EQ(output_idx, 0);
+  DCHECK(node.node().regressor().has_top_value());
+  return node.node().regressor().top_value();
+}
+
+double GetClassificationNodeWinnerTakeAllValue(
+    const model::decision_tree::NodeWithChildren& node, const int output_idx) {
+  return node.node().classifier().top_value() == (output_idx + 1);
+}
+
+double GetClassificationNodeNonWinnerTakeAllValue(
+    const model::decision_tree::NodeWithChildren& node, const int output_idx) {
+  DCHECK(node.node().classifier().has_distribution());
+  const auto& d = node.node().classifier().distribution();
+  return d.counts(output_idx + 1) / d.sum();
+}
+
+}  // namespace
+
+namespace {
+
+// Finds the first item in the patch with the given attribute idx. If not found,
+// returns -1.
+std::optional<int> FindFirst(const internal::Path& path,
+                             const int attribute_idx) {
+  for (int item_idx = 0; item_idx < path.size(); item_idx++) {
+    if (path[item_idx].column_idx == attribute_idx) {
+      return item_idx;
+    }
+  }
+  return std::nullopt;
+}
+
+}  // namespace
 
 namespace internal {
 
@@ -106,5 +169,283 @@ double unwound_sum(const int path_idx, Path& path) {
   }
   return total * (unwound_path_length + 1);
 }
+
+absl::Status recurse(const model::decision_tree::NodeWithChildren& node,
+                     const double zero_fraction, const double one_fraction,
+                     const int attribute_idx, const NodeValueFn node_value_fn,
+                     const bool multi_output_trees, const int tree_idx,
+                     const dataset::proto::Example& example, Path path,
+                     ExampleShapValues& shap_values) {
+  extend(zero_fraction, one_fraction, attribute_idx, path);
+
+  if (node.IsLeaf()) {
+    for (int path_idx = 1; path_idx < path.size(); path_idx++) {
+      const double weight = unwound_sum(path_idx, path);
+      const double scale =
+          weight * (path[path_idx].one_fraction - path[path_idx].zero_fraction);
+
+      if (multi_output_trees) {
+        for (int output_idx = 0; output_idx < shap_values.num_outputs();
+             output_idx++) {
+          const double node_value = node_value_fn(node, output_idx);
+          const int acc_index =
+              shap_values.Index(path[path_idx].column_idx, output_idx);
+          shap_values.values()[acc_index] += node_value * scale;
+        }
+      } else {
+        const int output_idx = tree_idx % shap_values.num_outputs();
+        const double node_value = node_value_fn(node, 0);
+        const int acc_index =
+            shap_values.Index(path[path_idx].column_idx, output_idx);
+        shap_values.values()[acc_index] += node_value * scale;
+      }
+    }
+  } else {
+    // Evaluate condition
+    ASSIGN_OR_RETURN(const bool eval, model::decision_tree::EvalCondition(
+                                          node.node().condition(), example));
+
+    auto hot_node = node.pos_child();
+    auto cold_node = node.neg_child();
+    if (!eval) {
+      std::swap(hot_node, cold_node);
+    }
+
+    // Called "k" in paper
+    const int new_attribute_idx = node.node().condition().attribute();
+    const auto path_index = FindFirst(path, new_attribute_idx);
+    // TODO: Support oblique splits properly. Currently, only the first
+    // attribute of the oblique splits gather all the SHAP values.
+
+    double incoming_zero_fraction = 1.;
+    double incoming_one_fraction = 1.;
+    if (path_index.has_value()) {
+      // The attribute already exists in the branch.
+      incoming_zero_fraction = path[*path_index].zero_fraction;
+      incoming_one_fraction = path[*path_index].one_fraction;
+      unwind(*path_index, path);
+    }
+
+    const double num_examples = GetNumNodeExamples(node);
+    const double hot_zero_fraction =
+        GetNumNodeExamples(*hot_node) / num_examples;
+    const double cold_zero_fraction =
+        GetNumNodeExamples(*cold_node) / num_examples;
+    DCHECK_GT(hot_zero_fraction, 0);
+    DCHECK_GT(cold_zero_fraction, 0);
+
+    RETURN_IF_ERROR(
+        recurse(*hot_node, incoming_zero_fraction * hot_zero_fraction,
+                incoming_one_fraction, new_attribute_idx, node_value_fn,
+                multi_output_trees, tree_idx, example, path, shap_values));
+
+    RETURN_IF_ERROR(
+        recurse(*cold_node, incoming_zero_fraction * cold_zero_fraction, 0.,
+                new_attribute_idx, node_value_fn, multi_output_trees, tree_idx,
+                example, path, shap_values));
+  }
+  return absl::OkStatus();
+}
+
+// Struct to get the output of leaf values.
+struct ModelAccessor {
+  // Access the leaf values.
+  internal::NodeValueFn node_value_fn;
+  // Scale SHAP values by this value.
+  double scale;
+  // Number of output of the model.
+  int num_outputs;
+  // If true, each tree contains values for all the outputs. If false, each tree
+  // contributes to a single output.
+  bool multi_output_trees;
+  // Tree accessor.
+  const model::DecisionForestInterface* decision_forest;
+  // Possibly null variable. If set, the zero-trees bias of the model.
+  const std::vector<float>* maybe_zero_tree_bias;
+};
+
+absl::StatusOr<ModelAccessor> GetModelAccessor(
+    const model::AbstractModel& model) {
+  auto* maybe_gbt = dynamic_cast<
+      const model::gradient_boosted_trees::GradientBoostedTreesModel*>(&model);
+  if (maybe_gbt) {
+    return ModelAccessor{
+        .node_value_fn = GetRegressionNodeValue,
+        .scale = 1.,
+        .num_outputs = maybe_gbt->num_trees_per_iter(),
+        .multi_output_trees = false,
+        .decision_forest = maybe_gbt,
+        .maybe_zero_tree_bias = &maybe_gbt->initial_predictions(),
+    };
+  }
+
+  auto* maybe_rf =
+      dynamic_cast<const model::random_forest::RandomForestModel*>(&model);
+  if (maybe_rf) {
+    switch (model.task()) {
+      case model::proto::Task::CLASSIFICATION: {
+        // TODO: Optimize for binary classification.
+        const int num_classes = model.data_spec()
+                                    .columns(model.label_col_idx())
+                                    .categorical()
+                                    .number_of_unique_values() -
+                                1;
+        return ModelAccessor{
+            .node_value_fn = maybe_rf->winner_take_all_inference()
+                                 ? GetClassificationNodeWinnerTakeAllValue
+                                 : GetClassificationNodeNonWinnerTakeAllValue,
+            .scale = 1. / maybe_rf->NumTrees(),
+            .num_outputs = num_classes,
+            .multi_output_trees = true,
+            .decision_forest = maybe_rf,
+            .maybe_zero_tree_bias = nullptr,
+        };
+      }
+      case model::proto::Task::REGRESSION:
+        return ModelAccessor{
+            .node_value_fn = GetRegressionNodeValue,
+            .scale = 1. / maybe_rf->NumTrees(),
+            .num_outputs = 1,
+            .multi_output_trees = true,
+            .decision_forest = maybe_rf,
+            .maybe_zero_tree_bias = nullptr,
+        };
+      default:
+        return absl::InvalidArgumentError(
+            absl::StrCat("SHAP is not implemented for task ", model.task(),
+                         " Random Forest models"));
+    }
+  }
+
+  // TODO: Add support for other models here.
+  return absl::InvalidArgumentError(
+      absl::StrCat("SHAP is not implemented for ", model.name(), " models"));
+}
+
+absl::Status GetModelBias(const ModelAccessor& accessor,
+                          std::vector<double>& bias) {
+  bias.assign(accessor.num_outputs, 0.);
+
+  double num_examples = 0.;
+  const int num_trees = accessor.decision_forest->decision_trees().size();
+  for (int tree_idx = 0; tree_idx < num_trees; tree_idx++) {
+    const auto& tree = accessor.decision_forest->decision_trees()[tree_idx];
+    const int output_idx_per_tree = tree_idx % accessor.num_outputs;
+
+    tree->IterateOnNodes([&accessor, &bias, &num_examples, output_idx_per_tree](
+                             const model::decision_tree::NodeWithChildren& node,
+                             const int depth) {
+      if (!node.IsLeaf()) {
+        return;
+      }
+      const auto num_examples_in_node = GetNumNodeExamples(node);
+      DCHECK_GT(num_examples_in_node, 0);
+      num_examples += num_examples_in_node;
+
+      if (accessor.multi_output_trees) {
+        for (int output_idx = 0; output_idx < accessor.num_outputs;
+             output_idx++) {
+          bias[output_idx] +=
+              accessor.node_value_fn(node, output_idx) * num_examples_in_node;
+        }
+      } else {
+        bias[output_idx_per_tree] +=
+            accessor.node_value_fn(node, 0) * num_examples_in_node;
+      }
+    });
+  }
+
+  // Normalize the accumulated biases according to the number of examples and
+  // extra scaling. If present, add the model zero-tree bias (after the
+  // scaling).
+  const double scale = num_trees * accessor.scale / num_examples;
+  for (int bias_idx = 0; bias_idx < bias.size(); bias_idx++) {
+    double& b = bias[bias_idx];
+    b *= scale;
+    if (accessor.maybe_zero_tree_bias) {
+      DCHECK_EQ(bias.size(), accessor.num_outputs);
+      b += (*accessor.maybe_zero_tree_bias)[bias_idx];
+    }
+  }
+
+  return absl::OkStatus();
+}
+
 }  // namespace internal
+
+void ExampleShapValues::Initialize(const int num_columns, const int num_outputs,
+                                   const bool compute_bias) {
+  num_outputs_ = num_outputs;
+  num_columns_ = num_columns;
+  values_.assign(num_columns * num_outputs, 0.);
+  if (compute_bias) {
+    bias_.assign(num_outputs, 0.);
+  }
+}
+
+double ExampleShapValues::SumValues(const int output_idx) const {
+  double sum = 0.;
+  for (int col_idx = 0; col_idx < num_columns_; col_idx++) {
+    sum += values_[Index(col_idx, output_idx)];
+  }
+  return sum;
+}
+
+std::string ExampleShapValues::ToString(
+    const dataset::proto::DataSpecification& data_spec) const {
+  std::string rep;
+  absl::StrAppend(&rep, "Values:\n");
+  for (int column_idx = 0; column_idx < num_columns(); column_idx++) {
+    const auto& col_name = data_spec.columns(column_idx).name();
+    absl::StrAppendFormat(&rep, "\t%s:", col_name);
+    for (int output_idx = 0; output_idx < num_outputs(); output_idx++) {
+      const auto value = values_[Index(column_idx, output_idx)];
+      absl::StrAppendFormat(&rep, " %f", value);
+    }
+    absl::StrAppendFormat(&rep, "\n");
+  }
+  absl::StrAppend(&rep, "Bias:\n\t", absl::StrJoin(bias_, ", "), "\n");
+  return rep;
+}
+
+absl::Status tree_shap(const model::AbstractModel& model,
+                       dataset::proto::Example& example,
+                       ExampleShapValues* shap_values, bool compute_bias) {
+  if (model.weights().has_value()) {
+    return absl::InvalidArgumentError(
+        "SHAP currently does not support weighted models");
+  }
+
+  ASSIGN_OR_RETURN(const auto accessor, internal::GetModelAccessor(model));
+
+  // Initialize the shap value accumulator.
+  shap_values->Initialize(model.data_spec().columns_size(),
+                          accessor.num_outputs, compute_bias);
+
+  if (compute_bias) {
+    RETURN_IF_ERROR(GetModelBias(accessor, shap_values->bias()));
+  }
+
+  // Transverse tree and populate the accumulator
+  internal::Path path;
+  const int num_trees = accessor.decision_forest->decision_trees().size();
+  for (int tree_idx = 0; tree_idx < num_trees; tree_idx++) {
+    const auto& tree = accessor.decision_forest->decision_trees()[tree_idx];
+    path.clear();
+    // Note: Unlike the paper, we use the magic value of -1 instead of 0 for
+    // "attribute_idx".
+    RETURN_IF_ERROR(internal::recurse(
+        tree->root(), 1., 1., -1, accessor.node_value_fn,
+        accessor.multi_output_trees, tree_idx, example, path, *shap_values));
+  }
+
+  // Normalize the values.
+  if (accessor.scale != 1.) {
+    for (double& value : shap_values->values()) {
+      value *= accessor.scale;
+    }
+  }
+  return absl::OkStatus();
+}
+
 }  // namespace yggdrasil_decision_forests::utils::shap
--- yggdrasil_decision_forests/utils/shap.h
+++ yggdrasil_decision_forests/utils/shap.h
@@ -20,14 +20,74 @@
 #ifndef YGGDRASIL_DECISION_FORESTS_UTILS_SHAP_H_
 #define YGGDRASIL_DECISION_FORESTS_UTILS_SHAP_H_
 
+#include <string>
 #include <vector>
 
+#include "absl/functional/function_ref.h"
+#include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/str_join.h"
+#include "yggdrasil_decision_forests/dataset/example.pb.h"
+#include "yggdrasil_decision_forests/model/abstract_model.h"
+#include "yggdrasil_decision_forests/model/decision_tree/decision_tree.h"
 
 namespace yggdrasil_decision_forests::utils::shap {
+
+// Container of SHAP values for a single example.
+class ExampleShapValues {
+ public:
+  // Creates an empty struct. Should be "Initialized" before any other
+  // operation.
+  ExampleShapValues() {}
+
+  // Initializes with all zero-values.
+  void Initialize(int num_columns, int num_outputs, bool compute_bias);
+
+  // Gets the index in "values()" for a given column and output.
+  int Index(const int column_idx, const int output_idx) const {
+    return output_idx + column_idx * num_outputs_;
+  }
+
+  // Shap values array of shape [num columns x num outputs].
+  std::vector<double>& values() { return values_; }
+  const std::vector<double>& values() const { return values_; }
+
+  // Biases array of shape [num outputs].
+  std::vector<double>& bias() { return bias_; }
+  const std::vector<double>& bias() const { return bias_; }
+
+  int num_outputs() const { return num_outputs_; }
+  int num_columns() const { return num_columns_; }
+
+  // Human readable string representation.
+  std::string ToString(
+      const dataset::proto::DataSpecification& data_spec) const;
+
+  // Sums the shap values for a given output.
+  double SumValues(int output_idx) const;
+
+ private:
+  std::vector<double> values_;
+  std::vector<double> bias_;
+  int num_outputs_ = 0;
+  int num_columns_ = 0;
+};
+
+// Computes the SHAP values using the TreeSHAP algorithm and save them
+// in "shap_values". The biases (e.g. "shap_values.bias()") is only computed
+// if "compute_bias = true".
+absl::Status tree_shap(const model::AbstractModel& model,
+                       dataset::proto::Example& example,
+                       ExampleShapValues* shap_values,
+                       bool compute_bias = true);
+
 namespace internal {
+
+// Signature of a function that get the "value" of a leaf node.
+typedef absl::FunctionRef<double(
+    const model::decision_tree::NodeWithChildren& node, const int output_idx)>
+    NodeValueFn;
 
 // PathItem used during in the TreeSHAP computation.
 struct PathItem {
@@ -67,6 +127,23 @@ void unwind(int path_idx, Path& path);
 // without the expensive operation of calling "unwind". This trick is not used
 // in the paper, but instead is used in the SHAP python package.
 double unwound_sum(int path_idx, Path& path);
+
+// Explores the structure of a tree and populates the shape values.
+// Note: The "recurse" function the TreeSHAP paper.
+// Note: "path" should NOT be a reference (this is the algorithm described in
+// the paper).
+// TODO: Optimize the algorithm and make "path" a reference i.e. avoid
+// copies.
+absl::Status recurse(
+    const model::decision_tree::NodeWithChildren& node,  // "j" in paper
+    double zero_fraction,                                // "p_z" in paper.
+    double one_fraction,                                 // "p_o" in paper.
+    int attribute_idx,                                   // "p_i" in paper.
+    NodeValueFn node_value_fn, bool multi_output_trees, int tree_idx,
+    const dataset::proto::Example& example,
+    Path path,                      // "m" in paper.
+    ExampleShapValues& shap_values  // "phi" in paper.
+);
 
 }  // namespace internal
 
